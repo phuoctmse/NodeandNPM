6767adedacd9f0f792bcff3dbb56d1645b0686a0
phuoctmse <phuoctmse184067@fpt.edu.vn>
2024-11-25 14:01:34 +0700

    use babel to compile code in old browsers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-import sum from './utils.js'
+import 'core-js/modules/es.object.values'
+import 'core-js/modules/es.promise'
+
+import sum from './utils'
 import './styles/style.css'
 import './styles/style.scss'
-console.log(sum(12,31))
+console.log(sum(100, 10))
+// ES6 Spread Operator
+const person = { name: 'Duoc' }
+const personClone = { ...person }
+console.log('personClone', personClone)
+
+// ES7 Object.values
+console.log('Object.values', Object.values(personClone))
--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,10 @@
 const sum = (a,b) => a + b;
+
+// ES6 Spread Operator
+const person = { name: 'Duoc' }
+const personClone = { ...person }
+console.log('personClone', personClone)
+
+// ES7 Object.values
+console.log('Object.values', Object.values(personClone))
 export default sum;
